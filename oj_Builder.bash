ac9ead0ba8da39394f9ff935bc2d227c60e1f739
1027800576@qq.com <1027800576@qq.com>
2021-11-04 16:45:21 +0800

    semantic:ASTbuilder,SemanticPre

--- oj_Builder.bash
+++
@@ -1,6 +0,0 @@
-# this script is called when the judge is building your compiler.
-# no argument will be passed in.
-# shellcheck disable=SC2121
-set ff=UNIX
-mkdir runContain
-find ./src -name "*.java" -print0|xargs -0 javac -d runContain -cp ./antlr-4.9.2-complete.jar
